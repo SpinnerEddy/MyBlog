b241d22500113243c332fc3e2335a43f974e1a37
SpinnerEddy <kiryu1022@gmail.com>
2026-07-20 18:15:33 +0900

    BlogPost.astroのprose化+TOC対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 // @ts-check
 
+import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import expressiveCode from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
@@ -9,7 +11,14 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    expressiveCode({
+      themes: ['github-light'],
+      plugins: [pluginLineNumbers()],
+    }),
+    mdx(),
+    sitemap(),
+  ],
 
   vite: {
     plugins: [tailwindcss()],
